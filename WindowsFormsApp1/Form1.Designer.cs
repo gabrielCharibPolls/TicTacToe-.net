04f7ac6b77ecfadb4b333750a3e78f89e11f2597
gabrielCharibPolls <gabrielCharibPolls>
2022-10-27 09:42:18 +0200

    addition of a simple interface

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button_00 = new System.Windows.Forms.Button();
+            this.button_01 = new System.Windows.Forms.Button();
+            this.button_02 = new System.Windows.Forms.Button();
+            this.button_10 = new System.Windows.Forms.Button();
+            this.button_11 = new System.Windows.Forms.Button();
+            this.button_12 = new System.Windows.Forms.Button();
+            this.button_20 = new System.Windows.Forms.Button();
+            this.button_21 = new System.Windows.Forms.Button();
+            this.button_22 = new System.Windows.Forms.Button();
+            this.labelActualPlayer = new System.Windows.Forms.Label();
+            this.labelCourentPlayer = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button_22);
+            this.panel1.Controls.Add(this.button_21);
+            this.panel1.Controls.Add(this.button_20);
+            this.panel1.Controls.Add(this.button_12);
+            this.panel1.Controls.Add(this.button_11);
+            this.panel1.Controls.Add(this.button_10);
+            this.panel1.Controls.Add(this.button_02);
+            this.panel1.Controls.Add(this.button_01);
+            this.panel1.Controls.Add(this.button_00);
+            this.panel1.Location = new System.Drawing.Point(25, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(411, 518);
+            this.panel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(462, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // button_00
+            // 
+            this.button_00.Location = new System.Drawing.Point(3, 30);
+            this.button_00.Name = "button_00";
+            this.button_00.Size = new System.Drawing.Size(117, 117);
+            this.button_00.TabIndex = 0;
+            this.button_00.UseVisualStyleBackColor = true;
+            // 
+            // button_01
+            // 
+            this.button_01.Location = new System.Drawing.Point(144, 30);
+            this.button_01.Name = "button_01";
+            this.button_01.Size = new System.Drawing.Size(117, 117);
+            this.button_01.TabIndex = 1;
+            this.button_01.UseVisualStyleBackColor = true;
+            // 
+            // button_02
+            // 
+            this.button_02.Location = new System.Drawing.Point(279, 30);
+            this.button_02.Name = "button_02";
+            this.button_02.Size = new System.Drawing.Size(117, 117);
+            this.button_02.TabIndex = 2;
+            this.button_02.UseVisualStyleBackColor = true;
+            // 
+            // button_10
+            // 
+            this.button_10.Location = new System.Drawing.Point(3, 170);
+            this.button_10.Name = "button_10";
+            this.button_10.Size = new System.Drawing.Size(117, 117);
+            this.button_10.TabIndex = 3;
+            this.button_10.UseVisualStyleBackColor = true;
+            // 
+            // button_11
+            // 
+            this.button_11.Location = new System.Drawing.Point(144, 170);
+            this.button_11.Name = "button_11";
+            this.button_11.Size = new System.Drawing.Size(117, 117);
+            this.button_11.TabIndex = 4;
+            this.button_11.UseVisualStyleBackColor = true;
+            // 
+            // button_12
+            // 
+            this.button_12.Location = new System.Drawing.Point(279, 170);
+            this.button_12.Name = "button_12";
+            this.button_12.Size = new System.Drawing.Size(117, 117);
+            this.button_12.TabIndex = 5;
+            this.button_12.UseVisualStyleBackColor = true;
+            // 
+            // button_20
+            // 
+            this.button_20.Location = new System.Drawing.Point(3, 313);
+            this.button_20.Name = "button_20";
+            this.button_20.Size = new System.Drawing.Size(117, 117);
+            this.button_20.TabIndex = 6;
+            this.button_20.UseVisualStyleBackColor = true;
+            // 
+            // button_21
+            // 
+            this.button_21.Location = new System.Drawing.Point(144, 313);
+            this.button_21.Name = "button_21";
+            this.button_21.Size = new System.Drawing.Size(117, 117);
+            this.button_21.TabIndex = 7;
+            this.button_21.UseVisualStyleBackColor = true;
+            // 
+            // button_22
+            // 
+            this.button_22.Location = new System.Drawing.Point(279, 313);
+            this.button_22.Name = "button_22";
+            this.button_22.Size = new System.Drawing.Size(117, 117);
+            this.button_22.TabIndex = 8;
+            this.button_22.UseVisualStyleBackColor = true;
+            // 
+            // labelActualPlayer
+            // 
+            this.labelActualPlayer.AutoSize = true;
+            this.labelActualPlayer.Location = new System.Drawing.Point(386, 24);
+            this.labelActualPlayer.Name = "labelActualPlayer";
+            this.labelActualPlayer.Size = new System.Drawing.Size(13, 13);
+            this.labelActualPlayer.TabIndex = 2;
+            this.labelActualPlayer.Text = "1";
+            // 
+            // labelCourentPlayer
+            // 
+            this.labelCourentPlayer.AutoSize = true;
+            this.labelCourentPlayer.Location = new System.Drawing.Point(343, 24);
+            this.labelCourentPlayer.Name = "labelCourentPlayer";
+            this.labelCourentPlayer.Size = new System.Drawing.Size(42, 13);
+            this.labelCourentPlayer.TabIndex = 3;
+            this.labelCourentPlayer.Text = "Player :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(462, 655);
+            this.Controls.Add(this.labelCourentPlayer);
+            this.Controls.Add(this.labelActualPlayer);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_22;
+        private System.Windows.Forms.Button button_21;
+        private System.Windows.Forms.Button button_20;
+        private System.Windows.Forms.Button button_12;
+        private System.Windows.Forms.Button button_11;
+        private System.Windows.Forms.Button button_10;
+        private System.Windows.Forms.Button button_02;
+        private System.Windows.Forms.Button button_01;
+        private System.Windows.Forms.Button button_00;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label labelActualPlayer;
+        private System.Windows.Forms.Label labelCourentPlayer;
     }
 }
 
