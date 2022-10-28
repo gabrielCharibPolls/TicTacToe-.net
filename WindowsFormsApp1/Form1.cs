86b593045148b74c4b96d76436d38810e89011b8
gabrielCharibPolls <gabrielcharib@gmail.com>
2022-10-28 21:22:01 +0200

    fix nextplayer code updateButtonWhenIsClick methode

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,34 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
 
 
-    
 
 
-    
+
+
     public partial class Form1 : Form
     {
 
         byte[,] tab = new byte[2, 2];
-        byte btCourentPlayer = 0; 
+        byte btCourentPlayer = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private static byte nextPayer(byte courentPlayer)
+        private void nextPayer()
         {
-            return (byte)(courentPlayer % 2);
+
+            btCourentPlayer = (byte)((btCourentPlayer % 2)+1);
+
         }
 
         ///////////////////////////////////////////////////
@@ -45,51 +40,77 @@ namespace WindowsFormsApp1
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+
+
+        private void updateButtonWhenIsClick(Button ActualeButton)
+        {
+            if (btCourentPlayer == 1)
+            {
+                ActualeButton.Text = "X";
+            }
+            else
+            {
+                ActualeButton.Text = "O";
+            }
+            nextPayer();
+
+        }
+
+
         private void button_00_Click(object sender, EventArgs e)
         {
+
             //changer les valeur dans le tableau 
             //changer le contenue du button 
             //changer de player 
+            //button_00.Text = "X";
+            updateButtonWhenIsClick((Button)sender);
         }
 
         private void button_01_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
+            
         }
 
         private void button_02_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
+            
         }
 
         private void button_10_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
+            
         }
 
         private void button_11_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
+           
         }
 
         private void button_12_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
+            
         }
 
         private void button_20_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
+            
         }
 
         private void button_21_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
         }
 
         private void button_22_Click(object sender, EventArgs e)
         {
-
+            updateButtonWhenIsClick((Button)sender);
         }
 
         private void buttonPlay_Click(object sender, EventArgs e)
